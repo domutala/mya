1fa59b2833535e074f89b7f6634e046fcb7b7194
Mamadou Talla DIA <ibntalla@gmail.com>
2025-02-16 05:34:04 +0000

    modifier le script de démarrage pour inclure la construction des dépendances et changer la commande de démarrage en production

--- start/service.sh
+++ start/service.sh
@@ -9,6 +9,7 @@ NPM_EXEC="/usr/bin/npm"
 
 cd $NODEJS_APP_PATH
 $NPM_EXEC install
+$NPM_EXEC build
 
 # Create the service file
 echo "Creating the systemd service..."
@@ -21,7 +22,7 @@ Description=Node.js Service for mya application
 After=network.target
 
 [Service]
-ExecStart=$NPM_EXEC run start:prod
+ExecStart=$NODEJS_EXEC -r tsconfig-paths/register dist/main.js
 WorkingDirectory=$NODEJS_APP_PATH
 Restart=always
 User=nobody  # You can adjust the user according to your setup
